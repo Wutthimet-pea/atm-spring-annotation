a530154962ef62bf1848af54a124d407245c76cd
wutthimet <wutthimet.pea@gmail.com>
2020-10-04 15:15:35 +0700

    61050815

---
+++ src/main/java/kmitl/ac/th/Main.java
@@ -0,0 +1,13 @@
+package kmitl.ac.th;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        ATM atm = context.getBean(ATM.class);
+
+        Bank bank = context.getBean(Bank.class);
+    }
+}
